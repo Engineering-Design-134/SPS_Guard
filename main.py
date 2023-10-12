00d7aee3ca0cb02feb96e12b542f7f9ff5f4774f
Jason Fu <gimpact709@gmail.com>
2023-10-12 14:47:57 +0200

    connection

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-import machine
-import time
+from machine import Pin, ADC
+import network
+from time import sleep
+import urequests as request
 
+flex_sensor = ADC(Pin(26))
+motor = Pin(15, Pin.OUT)
 
-flex_sensor = machine.ADC(machine.Pin(26, machine.Pin.IN))
-led = machine.Pin("LED", machine.Pin.OUT)
-led.off()
+api = "http://sps-api-ce9301a647f2.herokuapp.com"
+ssid = "Jason"
+password = "88888887"
+
+def connect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    for _ in range(10):
+        print('Waiting for connection...')
+        sleep(1)
+
+def get_settings():
+    url = api + f"/settings?device_id={1}"
+    response = request.get(url)
+    json = response.json()
+    return json
+
+connect()
+settings_file = open("settings.txt", "a")
+settings = get_settings()
+print(settings)
+settings_file.close()
 
 while True:
-    flex_value = flex_sensor.read_u16()
-    if flex_value > 100:
-        led.on()
+    flex = flex_sensor.read_u16()
+    print(flex)
+    if flex > 200:
+        motor.on()
     else:
-        led.off()
-    print(flex_value)
-    time.sleep(0.1)
+        motor.off()
