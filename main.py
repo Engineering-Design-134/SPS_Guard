686234001dde589e6617d4df788c3137c9bc43a4
Jason Fu <gimpact709@gmail.com>
2023-10-16 12:51:14 +0200

    async polling

--- main.py
+++ main.py
@@ -1,39 +1,71 @@
 from machine import Pin, ADC
 import network
-from time import sleep
 import urequests as request
+import uasyncio as asyncio
 
-flex_sensor = ADC(Pin(26))
-motor = Pin(15, Pin.OUT)
+flex_sensor = ADC(Pin(27))
+motor = Pin(14, Pin.OUT)
+
+test_led = Pin("LED", Pin.OUT)
+test_led.on()
 
 api = "http://sps-api-ce9301a647f2.herokuapp.com"
 ssid = "Jason"
 password = "88888887"
 
-def connect():
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid, password)
-    for _ in range(10):
-        print('Waiting for connection...')
-        sleep(1)
+connected = False
 
-def get_settings():
+flex_sensitvitity = 1000 # get local settings
+
+async def get_settings():
     url = api + f"/settings?device_id={1}"
+
     response = request.get(url)
     json = response.json()
     return json
 
-connect()
-settings_file = open("settings.txt", "a")
-settings = get_settings()
-print(settings)
-settings_file.close()
+async def update_settings():
+    global connected
+    global flex_sensitvitity
+    while connected:
+        try:
+            settings = await get_settings()
+            flex_sensitvitity = int(settings["flex_sensitivity"])
+        except:
+            await asyncio.sleep(5)
+            return
+        await asyncio.sleep(5)
 
-while True:
-    flex = flex_sensor.read_u16()
-    print(flex)
-    if flex > 200:
-        motor.on()
-    else:
-        motor.off()
+async def connect():
+    global flex_sensitvitity
+    global connected
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    while True:
+        wlan.connect(ssid, password)
+        print('Waiting for connection...')
+        await asyncio.sleep(3)
+        if wlan.isconnected():
+            print('Connected to network')
+            connected = True
+            await asyncio.create_task(update_settings())
+            connected = False
+
+
+async def work():
+    while True:
+        flex = flex_sensor.read_u16()
+        print(flex)
+        if flex > flex_sensitvitity:
+            motor.on()
+        else:
+            motor.off()
+        await asyncio.sleep(1)
+
+async def main():
+    work_task = asyncio.create_task(work())
+    connect_task = asyncio.create_task(connect())
+    await asyncio.get_event_loop().run_until_complete(asyncio.gather(work_task, connect_task))
+
+
+asyncio.run(main())
